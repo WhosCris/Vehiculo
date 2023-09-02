41b48ac6ef7130f2c8e3752d1c71675a71612255
WhosCris <cris.ibarra211@gmail.com>
2023-09-01 22:27:29 -0400

    Entrega

---
+++ src/vehiculo/Automovil.java
@@ -0,0 +1,85 @@
+package vehiculo;
+
+
+public class Automovil extends Vehiculo {
+   
+     private int cantidadPuertas;
+    
+
+    public Automovil( String marca, int año, double precio, int capacidadEstanque, int nivelEstanque,int cantidadPuertas) throws Exception {
+        super(marca, año, precio, capacidadEstanque, nivelEstanque);
+        this.cantidadPuertas = cantidadPuertas;
+        if (cantidadPuertas %2 ==0){
+             System.out.println("El numero es par ");
+        }else{
+            throw new Exception("la cantidad de puertas debe se par");
+        }
+    }
+    
+    public Automovil(){
+        
+    }
+    
+    public int getCantidadPuertas(){
+        return cantidadPuertas ;
+    }
+    
+    public void setCantidadPuertas (int cantidadPuertas){
+        this.cantidadPuertas = cantidadPuertas;
+    }
+    
+     @Override
+ public void CargarEstanque(int litros){
+     if (litros < 0) {
+         
+         System.out.println("El nivel del estanque es insuficiente cargar mas gasolina ");
+     }
+     if (getNivelEstanque() + litros >= getCapacidadEstanque()){
+          
+          System.out.println("No puede cargar mas gasolina ");
+      }else {
+          System.out.println("No puede cargar mas gasolina  ");
+      }
+
+ }
+  @Override
+ public void VaciarEstanqueo(int litros){
+     
+      if (getNivelEstanque()>15){
+         System.out.println("vaciando estanque ");
+     } else {
+         
+         try {
+             throw new Exception("El vehiculo no puede tener un nivel de estanque menor a 15");
+         } catch (Exception ex) {
+             ;
+         }
+     }   
+     
+     
+ }
+ @Override
+  public void calcularImpuesto(){
+     
+  if (getAño() < 2010){
+      
+      System.out.println(" usted no paga impuestos");
+  } else {
+      
+      super.calcularImpuesto();
+  }
+  
+ } 
+  
+     
+public void ModificarAño(int nuevoAño) {
+    
+        setAño(nuevoAño);
+       
+}
+      
+     
+    
+            
+}
+    
---
+++ src/vehiculo/Main.java
@@ -0,0 +1,25 @@
+package vehiculo;
+
+
+public class Main {
+
+  
+
+ 
+    public static void main(String[] args) throws Exception {
+        
+        Motocicleta moto1 = new Motocicleta("maria",2019,4000000,50,20,3);
+        Automovil carro1 = new Automovil("maria",2018,1000000,100,80,4);
+        
+       
+        moto1.ImprimirDatos();
+        moto1.CargarEstanque(12);
+        moto1.VaciarEstanqueo(1);
+        System.out.println("***********************************************");
+        carro1.ImprimirDatos();
+        carro1.VaciarEstanqueo(3);
+        carro1.CargarEstanque(34);
+        carro1.calcularImpuesto();
+    }
+    
+}
